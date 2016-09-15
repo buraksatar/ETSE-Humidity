bda8ac850572afcf2dbe0c887f49bd49773e9573
Burak Satar <burak.satar@ygaventures.org>
2016-09-15 19:14:49 +0300

    Create readAndParseDataforResistor.m

---
+++ readAndParseDataforResistor.m
@@ -0,0 +1,31 @@
+delete(instrfindall);
+clc;
+clear all;
+module = serial('COM15','BaudRate',115200,'DataBits',8);
+fopen(module)
+
+x = 0;
+
+while x<30
+    x=x+1
+    a = fscanf(module)
+    filteredData = textscan(a,'%s %d %s %d %s',5,'Delimiter',' ')
+       
+    y(x) = filteredData{2}    
+    tf = strjoin(filteredData{5})   
+    tf = tf(3:end)
+    resistorData(x) = str2num(tf)
+    
+    drawnow
+    plot(resistorData)
+    grid on
+    title('Reading data from module')
+    xlabel('Time')
+    ylabel('Data from ADC')
+    pause(0.1)  
+    
+end
+    
+fclose(module)
+delete(module)
+clear module
