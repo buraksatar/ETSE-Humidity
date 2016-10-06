ec80f1a86d7180a52fa3f42ed86a0941f538a420
Burak Satar <burak.satar@ygaventures.org>
2016-10-06 12:05:46 +0300

    Create united3.js

---
+++ clientSide/nodejsToMysql/United/united3.js
@@ -0,0 +1,35 @@
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var mysql = require('mysql');
+
+client.on('connect', function () {
+  client.subscribe('/data/+')
+})
+ 
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'HADIBAKALIM',
+  database: 'datas'
+});
+connection.connect();
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+  
+  console.log(message.toString())
+
+  var dataArray = {
+    temp: message.toString()
+  };
+
+var query = connection.query('insert into newDatas set ?', dataArray, function (err, result) {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.error(result);
+});
+
+})
