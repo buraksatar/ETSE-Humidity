dfe18c9377ad5871eac6f351b0c8125ab2509d8f
Burak Satar <burak.satar@ygaventures.org>
2016-10-14 15:21:13 +0200

    Update server3.js

--- clientSide/nodejsToWeb/ExpressJS/server3.js
+++ clientSide/nodejsToWeb/ExpressJS/server3.js
@@ -12,13 +12,8 @@ database: "datas"
 });
 
 //var test = "NABER LAN";
-
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
-// use res.render to load up an ejs view file
-
-// index page 
 
 
 // about page 
@@ -27,14 +22,11 @@ app.get('/about', function(req, res) {
 });
 
 
-
 var query = connection.query('SELECT * FROM sensor1;', function (error, rows, fields) 
 {    	
 	//console.log(str);
 	//console.log(rows.length + ' ROWS found');
-
-	
-
+	// index page
 	app.get('/', function(req, res) {
 
     		var sensors = [
@@ -42,13 +34,14 @@ var query = connection.query('SELECT * FROM sensor1;', function (error, rows, fi
       			{ name: 'data2', row: 2 },
         		{ name: 'data3', row: 3 }
     		];
-    		var tagline = str;
+    		var tagTemp = str;
 
     		var abc = "Just trying";
-    	
+		
+		// use res.render to load up an ejs view file
     		res.render('pages/index', {
         		sensors: sensors,
-        		tagline: tagline,
+        		tagTemp: tagTemp,
         		abc: abc
     		});
 	});
@@ -58,11 +51,11 @@ var query = connection.query('SELECT * FROM sensor1;', function (error, rows, fi
 	for(i=0;i<rows.length;i++)
      		str = str + rows[i].temp +'\n';
 
-
 	//var str= rows[1].temp;
 
 });
+
 //console.log(query.sql.toString());
 
 app.listen(8080);
-console.log('8080 is the magic port');
+console.log('8080 is the port');
