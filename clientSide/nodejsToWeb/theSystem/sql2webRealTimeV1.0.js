6cfb1134d30d406b4d7c7bb0983f9d5b2608451c
Burak Satar <burak.satar@ygaventures.org>
2016-12-07 15:41:35 +0100

    Create sql2webRealTimeV1.0.js

---
+++ clientSide/nodejsToWeb/theSystem/sql2webRealTimeV1.0.js
@@ -0,0 +1,126 @@
+//load the things we need
+var mqtt = require('mqtt')
+var client  = mqtt.connect('mqtt://test.mosquitto.org')
+var mysql = require('mysql');
+var express = require('express');
+var app = express();
+
+//make client on
+client.on('connect', function () {
+  //sensor1
+  client.subscribe('/data/8794332')
+  //sensor2
+  client.subscribe('/data/1496449')
+  //sensor3
+  client.subscribe('/data/1505613')
+})
+ 
+//database settings
+var connection = mysql.createConnection({
+  	host: 'localhost',
+  	user: 'root',
+  	password: 'HADIBAKALIM',
+  	database: 'datas',
+  	multipleStatements: true
+});
+
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+//main page
+app.get('/', function(req, res) {
+		// use res.render to load up an ejs view file
+    		res.render('pages/index');
+	});
+
+//express ajax
+app.use('/public', express.static('public'));
+
+
+
+
+connection.connect();
+
+
+//mqtt client is on
+client.on('message', function (topic, message) {
+  // message is Buffer
+
+        //if topic is sensor1
+        if(topic == '/data/8794332') {
+      
+                //get data as a string 
+                data1 = message.toString(); 
+      
+                //split it by comma
+                var newbuff1 = data1.split(",");
+                console.log(newbuff1);
+                //assign the data to related post1 values   
+                var post1  = {
+                        temp: newbuff1[0], 
+                        res: newbuff1[1], 
+                        adc: newbuff1[2], 
+                        mux: newbuff1[3]
+                };
+   
+                // put post1 values to table of sensor1 in MYSQL database
+                var query = connection.query('INSERT INTO sensor1 SET ?', post1 , function (err, result) {
+      		        if (err) {
+         		        console.error(err);
+        		        return;
+      		        }
+      		        console.error(result);
+                });        
+
+
+
+	        //PART OF SQLTOWEB, PART OF SQLTOWEB, PART OF SQLTOWEB
+                connection.query('SELECT * FROM sensor1 ; SELECT * FROM material', function (error1, rows1, fields1)
+                {    	
+		        // sensor1 page
+	                app.get('/RealTimeDeneme2', function(req, res1) {
+		                // use res.render to load up an ejs view file
+    		                res1.render('pages/RealTimeDeneme2', {
+        		                tagTemp1: tagTemp1,
+			                tagRes1: tagRes1,
+			                tagAdc1: tagAdc1,
+			                tagMux1: tagMux1,
+			                tagId1: tagId1
+    		                });
+	                });
+
+	                var tagTemp1 =[]; 
+	                for(var i=0;i<rows1[0].length;i++){
+     		                tagTemp1.push(rows1[0][i].temp);
+                        }
+ 
+	                var tagRes1=[];
+	                for(var i=0;i<rows1[0].length;i++){
+     		                tagRes1.push(rows1[0][i].res);
+                        }
+
+	                var tagAdc1=[]; 	   
+	                for(var i=0;i<rows1[0].length;i++){
+     		                tagAdc1.push(rows1[0][i].adc);
+                        }
+
+	                var tagMux1=[]; 
+	                for(var i=0;i<rows1[0].length;i++){
+     		                tagMux1.push(rows1[0][i].mux);
+                        }
+  	
+                        var tagId1=[];	   
+	                for(var i=0;i<rows1[0].length;i++){
+     		                tagId1.push(rows1[0][i].id);
+                        }
+	                console.log(tagRes1);
+                });
+
+        }
+
+})
+
+
+app.listen(8080);
+console.log('the port: 8080, is active ');
