479aeb3348250267c9cb70c591e96f81fd4525a3
Burak Satar <burak.satar@ygaventures.org>
2017-02-22 13:25:47 +0100

    Update config.lua

--- clientSide/mqtt/Send-Recieve-Test/config.lua
+++ clientSide/mqtt/Send-Recieve-Test/config.lua
@@ -3,8 +3,10 @@ local module = {}
 module.SSID = {}  
 module.SSID["gpdswpa"] = "puercoespin"
 
-module.HOST = "test.mosquitto.org"  
-module.PORT = 1883  
+module.HOST = "147.156.81.149"  
+module.PORT = 1883
+module.USERNAME = "burak"
+module.PASSWORD = "hadibakalim"  
 module.ID = node.chipid()
 print ("Module ID "..node.chipid())
        
