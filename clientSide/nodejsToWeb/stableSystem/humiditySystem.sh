7041d55a1ed34946d49d04dc66c9a78748c7701c
Burak Satar <burak.satar@ygaventures.org>
2017-02-28 17:54:25 +0100

    Update humiditySystem.sh

--- clientSide/nodejsToWeb/stableSystem/humiditySystem.sh
+++ clientSide/nodejsToWeb/stableSystem/humiditySystem.sh
@@ -9,3 +9,8 @@ sudo service mosquitto start
 echo "Designed for ETSE Valencia by Burak Satar"
 
 # installed in /etc/init.d/
+# to make the script executable write this
+# sudo chmod humiditySystem.sh
+#
+# and to make it auto-start with every start
+# sudo update-rc.d humiditySystem.sh defaults
